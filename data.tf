23c25b1402969832b598db6fb1789b61b7b3d6da
CodingManoj <mvp2612@hotmail.com>
2023-09-24 23:11:26 -0400

    Git Auto Push

--- data.tf
+++ data.tf
@@ -1,7 +1,7 @@
 # Reads the information from the remote statefile
 data "terraform_remote_state" "vpc" {
   backend = "s3"
-  config {
+  config = {
     bucket = "b55-terraform-state"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"

d27d2a811d6afeda2cda60c05ad679e7182ac1d8
CodingManoj <mvp2612@hotmail.com>
2023-09-27 23:07:34 -0400

    Git Auto Push

--- cluster.tf
+++ cluster.tf
@@ -14,7 +14,7 @@ resource "aws_docdb_cluster_instance" "cluster_instances" {
   count              = 1
   identifier         = "roboshop-${var.ENV}-docdb"
   cluster_identifier = aws_docdb_cluster.docdb.id
-  instance_class     = "db.t3.medium"
+  instance_class     = var.DOCDB_INSTANCE_TYPE
 }
 
 # Creates DocDB Subnet Group
